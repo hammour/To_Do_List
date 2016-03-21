bb788e99f9fd029b2bcd23f9bf2d70804e772b92
Haythem Hammour <hammour@gmail.com>
2016-03-20 21:24:19 -0500

    Explorer mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 var taskData = [];
 var newTaskEl = document.getElementById('newTask');
 var btnOneEl = document.getElementById('btnOne');
+var showTasks = document.getElementById('showTasks');
 
 
 
 
+
+function clearhField(){
+	newTask.placeholder='News Task...';
+}
 
 function addNewTask(whatToDo){
 	
@@ -12,10 +17,28 @@ function addNewTask(whatToDo){
 	
 	}
 
+function displayTasks(){
+
+	for (var i=taskData.length-1; i<taskData.length;i++){
+		
+		showTasks.insertAdjacentHTML('afterend', '<h3><span class=\'listNumbers\'> No.'+(i+1)+'</span>- '+taskData[i].task)+'</h3>';
 
 
+	}
+	//showTasks=' ';
+	//showTasks.innerHTML;
+}
 
-btnOneEl.addEventListener('click', function(){taskData.push(addNewTask(newTaskEl.value))},false);
+//showTasks.innerHTML;
+btnOneEl.addEventListener('click', 
+	function(){
+
+	taskData.push(addNewTask(newTaskEl.value));
+	displayTasks();
+	newTask.value=null;
+
+			},
+			false); 
 
 // add that object to an an array of objects tasks data base.
 
